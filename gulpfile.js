d6ed04921b8b668e0b0adbad6cd564c190db66ef
cartolari <bruno.cartolari@gmail.com>
2014-11-28 15:23:07 -0200

    Respawn gulp on compilation errors

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,13 @@ var coffee = require('gulp-coffee');
 var paths = {
   sass: ['./www/**/*.scss'],
   coffee: ['./www/**/*.coffee']
-
 };
+
+function handleError(error) {
+  var displayErr = gutil.colors.red(error);
+  gutil.log(displayErr);
+  process.exit(1);
+}
 
 gulp.task('default', ['sass']);
 
@@ -27,13 +32,25 @@ gulp.task('sass', function(done) {
 
 gulp.task('coffee', function(done) {
   gulp.src(paths.coffee)
-  .pipe(coffee({bare: true}).on('error', gutil.log))
+  .pipe(coffee({bare: true}).on('error', handleError))
   .pipe(concat('application.js'))
   .pipe(gulp.dest('./www/js'))
-  .on('end', done)
-})
+  .on('end', done);
+});
 
 gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass'])
-  gulp.watch(paths.coffee, ['coffee'])
+  gulp.watch(paths.sass, ['sass']).on('error', handleError);
+  gulp.watch(paths.coffee, ['coffee']).on('error', handleError);
 });
+
+gulp.task('spawn-watch', function() {
+  var spawnWatch = function() {
+    var proc = require('child_process').spawn('gulp', ['watch'], {stdio: 'inherit'});
+    proc.on('close', function (code) {
+      spawnWatch();
+    });
+  };
+  spawnWatch();
+});
+
+gulp.task('default', ['spawn-watch']);
