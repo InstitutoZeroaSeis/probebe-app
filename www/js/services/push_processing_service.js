5d698b6f3f3892dcfeab290e3992abee0d1273c6
cartolari <bruno.cartolari@gmail.com>
2014-12-04 21:50:17 -0200

    Refactor push processing service

--- www/js/services/push_processing_service.js
+++ www/js/services/push_processing_service.js
@@ -1,33 +1,30 @@
 angular.module("proBebe.services").factory('pushProcessing', function($rootScope, $cordovaPush, $ionicPlatform, Constants, authentication) {
-  return {
-    initialize: function() {
+  var PushProcessing = function() {
+    var pushNotification = function(event, notification) {
+      var elem, injector, myService;
+      console.info("pushNotificationReceived. Notification argument: " + JSON.stringify(notification));
+
+      if (notification.event === 'registered') {
+        authentication.registerDeviceNotificationId(notification.regid);
+      } else if (notification.event === 'message') {
+        window.open(notification.payload.article_url, "_system");
+      }
+    };
+
+    this.initialize = function() {
       var pushConfig = { senderID: Constants.PUSH_NOTIFICATION.GCM.SENDER_ID };
+
       $ionicPlatform.ready(function() {
         $cordovaPush.register(pushConfig).then(function(result) {
           console.info("Push registered, result = " + result);
-        }, function(error) {
+        }).catch(function (error) {
           console.error("Registration failed, error = " + error);
         });
       });
-      $rootScope.$on('pushNotificationReceived', function(event, notification) {
-        var elem, injector, myService;
-        console.log("EVENT RECEIVED: " + notification.event + " ");
-        switch (notification.event) {
-          case 'registered':
-            console.log("REGISTERED with GCM Server REGID: " + notification.regid);
-            authentication.registerDeviceNotificationId(notification.regid);
-            break;
-          case 'message':
-            console.log("Received message " + notification.message + ", payload = " + notification.payload);
-            window.open(notification.payload.article_url, "_system");
-            break;
-          case 'error':
-            console.log("ERROR MSG: " + notification);
-            break;
-          default:
-            console.log("Unknown event sent: " + event);
-        }
-      });
-    }
+
+      $rootScope.$on('pushNotificationReceived', pushNotification);
+    };
   };
+
+  return new PushProcessing();
 });
