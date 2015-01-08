12a5923e93eb0c934b378b4215de19afa0e96b09
cartolari <bruno.cartolari@gmail.com>
2015-01-07 22:03:25 -0200

    Pick a child from the left menu

--- www/js/controllers/messages_ctrl.js
+++ www/js/controllers/messages_ctrl.js
@@ -4,7 +4,14 @@ controllers = angular.module("proBebe.controllers");
 
 controllers.controller("MessagesCtrl", function($scope, $ionicPopup, Child) {
   function getChildren() {
-    $scope.children = Child.query();
+    $scope.children = Child.query(function() {
+      if (!$scope.selectedChild) {
+        $scope.selectedChild = $scope.children[0];
+      }
+    });
+  }
+  $scope.selectChild = function(child) {
+    $scope.selectedChild = child;
   }
 
   $scope.$on('pushMessageReceived', getChildren);
