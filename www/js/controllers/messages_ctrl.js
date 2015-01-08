0bf1e242e17db67d0c8b366539ee56b031868537
cartolari <bruno.cartolari@gmail.com>
2015-01-08 14:40:39 -0200

    Fix selecting children

--- www/js/controllers/messages_ctrl.js
+++ www/js/controllers/messages_ctrl.js
@@ -2,7 +2,7 @@ var controllers;
 
 controllers = angular.module("proBebe.controllers");
 
-controllers.controller("MessagesCtrl", function($scope, $ionicPopup, Child) {
+controllers.controller("MessagesCtrl", function($scope, $rootScope, $ionicPopup, Child) {
   function getChildren() {
     $scope.children = Child.query(function() {
       if (!$scope.selectedChild) {
@@ -10,9 +10,15 @@ controllers.controller("MessagesCtrl", function($scope, $ionicPopup, Child) {
       }
     });
   }
+
   $scope.selectChild = function(child) {
     $scope.selectedChild = child;
+    $rootScope.$emit('childSelected', child);
   }
+
+  $scope.$on('childSelected', function(child) {
+    $scope.selectedChild = child;
+  });
 
   $scope.$on('pushMessageReceived', getChildren);
   getChildren();
