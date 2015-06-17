1d92039064dd156dc4f7cfe5b8aa2d7855561b5b
diegonakamashi <diegonakamashi@gmail.com>
2015-06-16 21:02:35 -0300

    update ionic to 1.0.0

--- www/js/controllers/show_message_ctrl.js
+++ www/js/controllers/show_message_ctrl.js
@@ -4,4 +4,8 @@ controllers = angular.module("proBebe.controllers");
 
 controllers.controller("ShowMessageCtrl", function($scope, $stateParams, Message) {
   $scope.message = Message.get({id: $stateParams.id});
+
+  $scope.toggle = function(){
+    alert('aaa');
+  }
 });
