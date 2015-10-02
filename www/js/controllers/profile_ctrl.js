2899507e0f119d932b781f57cb58bd59741708e6
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2015-10-02 15:05:34 -0300

    Base layout to profile page

--- www/js/controllers/profile_ctrl.js
+++ www/js/controllers/profile_ctrl.js
@@ -1,8 +1,13 @@
 angular.module("proBebe.controllers")
 .controller("ProfileCtrl", function($scope, $ionicLoading, $state, $http, Constants, authentication, storage) {
   $scope.profile = {
-    name: authentication.name()
+    name: authentication.name(),
+    sons: [{name: "", bornDate: "", sex: ""}]
   };
+
+  $scope.addSon = function(){
+    $scope.profile.sons.push({name: "", bornDate: "", sex: ""})
+  }
 
 });
 
