4a3fd933ddb0a6b52a2bf74d77761dfcf5e06f1b
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2015-10-22 16:54:39 -0200

    Refactoring auth_ctrl and definition of message handler

--- www/js/controllers/auth_ctrl.js
+++ www/js/controllers/auth_ctrl.js
@@ -1,79 +1,67 @@
-(function() {
-  function showLoading(ionicLoading, text) {
-    ionicLoading.show({
-      template: text,
-      noBackdrop: true,
-      duration: 4000
-    });
+angular.module("proBebe.controllers")
+.controller("AuthCtrl", function($scope, $state, $http, Constants, authentication, storage, errorHandler, messageHandler) {
+  $scope.login_info = {};
+  $scope.user = {};
+  $scope.form = {
+    signin: true,
+    signup: false
   }
 
-  angular.module("proBebe.controllers")
-  .controller("AuthCtrl", function($scope, $ionicLoading, $state, $http, Constants, authentication, storage, errorHandler) {
-    $scope.login_info = {};
-    $scope.user = {};
-    $scope.form = {
-      signin: true,
-      signup: false
-    }
+  $scope.toggleTab = function(){
+    $scope.form.signin = !$scope.form.signin;
+    $scope.form.signup = !$scope.form.signup;
+  }
 
-    function defineData(){
-      return {
-        user: { profile_attributes: {name: $scope.user.name},
-          email: $scope.user.email,
-          password: $scope.user.password,
-          source: ""
-        }
+  $scope.signIn = function(state) {
+    var authPromise = authentication.authenticate($scope.login_info.email, $scope.login_info.password, $scope.login_info.name);
+    var loading = messageHandler.showWithTemplate();
+
+    return authPromise.then(function(result) {
+      loading.hide();
+      if (result) {
+        messageHandler.show("Autenticado com sucesso");
+        $state.go(state);
+      } else {
+        messageHandler.show("Credenciais inválidas");
+      }
+    }).catch(function(error) {
+      messageHandler.show("Ocorreu um erro na autenticação");
+    });
+  };
+
+  $scope.signUp = function(form) {
+    var loading = messageHandler.showWithTemplate();
+
+    if (form.$valid) {
+      var data = defineData();
+      $http.post(Constants.SIGN_UP_URL, data).then(function(result) {
+        $scope.login_info.email = $scope.user.email;
+        $scope.login_info.password = $scope.user.password;
+        $scope.login_info.name = $scope.user.name;
+        loading.hide();
+        $scope.signIn('profile');
+      }).catch(function(response) {
+        loading.hide();
+        messageHandler.show(errorHandler.message(response));
+      });
+    }
+  };
+
+  $scope.signOut = function() {
+    authentication.signOut();
+    storage.clear();
+    $state.go('sign');
+  };
+
+  function defineData(){
+    return {
+      user: { profile_attributes: {name: $scope.user.name},
+        email: $scope.user.email,
+        password: $scope.user.password,
+        source: ""
       }
     }
+  }
 
-    $scope.toggleTab = function(){
-      $scope.form.signin = !$scope.form.signin;
-      $scope.form.signup = !$scope.form.signup;
-    }
+});
 
-    $scope.signIn = function(state) {
-      var authPromise = authentication.authenticate($scope.login_info.email, $scope.login_info.password, $scope.login_info.name);
-      $ionicLoading.show({
-        templateUrl: 'templates/loading.html'
-      });
-      return authPromise.then(function(result) {
-        $ionicLoading.hide();
-        if (result) {
-          showLoading($ionicLoading, "Autenticado com sucesso");
-          $state.go(state);
-        } else {
-          showLoading($ionicLoading, "Credenciais inválidas");
-        }
-      }).catch(function(error) {
-        showLoading($ionicLoading, "Ocorreu um erro na autenticação");
-      });
-    };
-
-    $scope.signUp = function(form) {
-      $ionicLoading.show({
-        templateUrl: 'templates/loading.html'
-      });
-      if (form.$valid) {
-        var data = defineData();
-        $http.post(Constants.SIGN_UP_URL, data).then(function(result) {
-          $scope.login_info.email = $scope.user.email;
-          $scope.login_info.password = $scope.user.password;
-          $scope.login_info.name = $scope.user.name;
-          $ionicLoading.hide();
-          $scope.signIn('profile');
-        }).catch(function(response) {
-          $ionicLoading.hide();
-          var messageError = errorHandler.message(response);
-          showLoading($ionicLoading, messageError);
-        });
-      }
-    };
-
-    $scope.signOut = function() {
-      authentication.signOut();
-      storage.clear();
-      $state.go('sign');
-    };
-
-  });
-})();
---
+++ www/js/services/message_handler.js
@@ -0,0 +1,25 @@
+angular.module("proBebe.services")
+.factory('messageHandler', function($ionicLoading) {
+
+  function Handler() {}
+
+  Handler.show = function(message){
+    $ionicLoading.show({
+      template: message,
+      noBackdrop: true,
+      duration: 4000
+    });
+    return $ionicLoading;
+  }
+
+  Handler.showWithTemplate = function(){
+    $ionicLoading.show({
+      templateUrl: 'templates/loading.html'
+    });
+    return $ionicLoading;
+  }
+
+
+  return Handler;
+
+});
