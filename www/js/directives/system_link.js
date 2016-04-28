abea784f3a52c603b896a076dca09a5b53bf5207
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2016-04-28 19:58:04 -0300

    Fixing bug to open link at system bronwser'

---
+++ www/js/directives/system_link.js
@@ -0,0 +1,7 @@
+app.filter('hrefToJS', function ($sce, $sanitize) {
+    return function (text) {
+        var regex = /href="([\S]+)"/g;
+        var newString = $sanitize(text).replace(regex, "href=\"#\" onClick=\"window.open('$1', '_system', 'location=yes');return false;\"");
+        return $sce.trustAsHtml(newString);
+    }
+});
